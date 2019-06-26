3c5d15f89049f4e6c8acf4e40ceb70b374ecdfbf
Schluffe <schluffe@localhost.localdomain>
2019-06-27 01:10:19 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-const Discord = require('discord.js');
 const HellBot = require('./hellbot');
 const config = require('./config.json');
 
-const client = new Discord.Client();
-const bot = new HellBot(config);
-
-bot.command.ping = (message) => {
-	message.channel.send('pong!');
-}
-
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', message => {
-  if ( message.content.startsWith(bot.config.prefix) ) {
-		try {
-			const command = message.content.split(' ', 1)[0].substr(1);
-			bot.command[command](message);
-		}
-		catch (error) {
-			console.log(error);
-			message.reply(`Unknown command: '${message}'`);
-		}
-  }
-});
-
-client.login(bot.config.token);
+const hellBot = new HellBot(config);
+hellBot.run();
--- hellbot.js
+++ hellbot.js
@@ -1,11 +1,53 @@
+const Discord = require('discord.js');
+const { ping, sleep, wakeup } = require('./commands.js');
+
 class HellBot {
 	constructor(config) {
-		this.config = {
+		this.client = new Discord.Client();
+		this.config = this.parseConfig(config);
+		this.command = {
+			ping: ping,
+			sleep: sleep,
+			wakeup: wakeup,
+		};
+	}
+
+	run() {
+		this.client.once('ready', () => {
+			console.log(`Logged in as: ${this.client.user.tag}`);
+		});
+
+		this.client.on('message', message => {
+			if ( message.content.startsWith(this.config.prefix) && !message.author.bot ) {
+				try {
+					this.command[this.parseCommand(message)](message);
+				}
+				catch (error) {
+					console.log(error);
+				}
+			}
+		});
+
+		this.client.login(this.config.token);
+	}
+
+	parseConfig(config) {
+		return {
 			prefix: config.prefix,
 			token: config.token,
 		};
+	}
 
-		this.command = {};
+	parseCommand(message) {
+		return message.content.split(' ', 1)[0].substr(this.config.prefix.length);
+	}
+
+	registerCommand(name, callback) {
+		if ( this.command[name] !== undefined ) {
+			throw new Error(`Command: '${name}', already exists!`);
+		}
+
+		this.command[name] = callback;
 	}
 }
 
