2b06f4a5a35cc1b18df91c1a96900bf994d6b4ff
kolaflash <kolaflash@TREK>
2020-08-13 12:09:20 +0200

    Refactor ping

--- commands/ping/ping.js
+++ commands/ping/ping.js
@@ -1,13 +1,16 @@
 const Command = require('../../src/command');
 
-function Ping() {
-    Command.call(this);
-    this.trigger.push('ping');}
+class Ping extends Command {
+    constructor(hellbot) {
+        super(hellbot);
+        this.trigger.push('ping');
+    }
 
-Ping.prototype.execute = function(args, message, { i18n, store }) {
-    const locale = store.get('users').get(message.author.id).locale;
-    const reply = i18n.t(locale, 'ping.pong');
-    message.reply(reply);
+    execute(args, message, { i18n, store }) {
+        const locale = store.get('users').get(message.author.id).locale;
+        const reply = i18n.t(locale, 'ping.pong');
+        message.reply(reply);
+    }
 }
 
 module.exports = Ping;
