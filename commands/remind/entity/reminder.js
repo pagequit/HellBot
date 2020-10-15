343e40c42afd8f1b9f23b84bada7ce994fdeb529
kolaflash <kolaflash@TREK>
2020-10-15 21:21:43 +0200

    Add latest changes

--- commands/remind/entity/reminder.js
+++ commands/remind/entity/reminder.js
@@ -1,5 +1,19 @@
+const CronJob = require('cron').CronJob;
+
 module.exports = function Reminder(data) {
-    this.title = data.title;
-    this.timestamp = data.timestamp;
-    this.hookk = data.hook; // this might be a nice feature in the future :D
+    const date = new Date();
+    this.date = date.setMinutes(date.getMinutes() + data.minutes);
+    this.subject = data.subject;
+    this.hellUser = data.hellUser;
+}
+
+Reminder.prototype = {
+    start({ ext }) {
+        new CronJob(this.date, () => {
+            const user = ext.$store.get('guild').members.cache
+                .find(m => m.id === this.hellUser.id).user;
+            
+            user.send(ext.$i18n.t(this.user.locale, 'remmind.default', [user.username, this.subject]));
+        });
+    },
 }
--- commands/remind/remind.js
+++ commands/remind/remind.js
@@ -1,7 +1,7 @@
 const { DiscordAPIError } = require('discord.js');
 const Command = require('../../src/command');
 const { Collection } = require('discord.js');
-
+const Reminder = require('./entity/reminder');
 
 class Remind extends Command {
 	constructor() {
@@ -18,8 +18,23 @@ class Remind extends Command {
 
 		const locale = hellUser.locale;
 
+		if (args.length < 2) {
+			message.reply(`I need a subject and a termin in order to remind you.`);
+			return;
+		}
+
+		const subject = args[0];
+		const minutes = args[1];
+
 		if (this.reminder.some(hellUser.id)) {
 			message.reply(`I should remind you already to ${this.reminder.get(hellUser.id).title}`);
+		}
+		else {
+			this.reminder.set(hellUser.id, new Reminder({
+				subject: subject,
+				minutes: minutes,
+				hellUser: hellUser,
+			}));
 		}
 
 	}
