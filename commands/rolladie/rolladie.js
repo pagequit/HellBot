5f85208d6b865ee402f6c61cf51f752fbb4ca967
gambit <gambit@CHANGE>
2021-06-13 11:46:51 +0200

    Refactor the rollADie command

--- commands/rolladie/rolladie.js
+++ commands/rolladie/rolladie.js
@@ -23,7 +23,7 @@ class RollADie extends Command {
 
 			if (arg[0] === 'd') {
 				result.value = this.rollDie(arg.substr(1));
-				result.string = `[${result.value}:${arg.substr(1)}]`;
+				result.string = `[ ${result.value} ]`;
 
 				return result;
 			}
@@ -33,55 +33,41 @@ class RollADie extends Command {
 			while(quiantifier-- > 0) {
 				const value = this.rollDie(die);
 				result.value += value;
-				result.string += `[${value}:${die}]`;
+				result.string += `[ ${value} ]`;
 			}
 
 			return result;
 		}
 
-		const regex = /^\d?d\d+$/g;
 		const result = formula.reduce((acc, cur) => {
-			if (!cur.match(regex)) {
-				return;
-			}
 			const resObj = resolve.call(this, cur);
 			acc.sum += resObj.value;
-			acc.string += ` ${resObj.string}`;
+			acc.string += `, ${resObj.string}`;
 			return acc;
 		}, {
 			sum: 0,
 			string: '',
 		});
 
-		return `Σ(${result.sum})` + result.string;
+		return `Σ( ${result.sum} )` + result.string;
 	}
 
 	async execute(args, message) {
 		const prismaUser = await this.$prisma.getPrismaUserById(message.author.id);
 		const locale = prismaUser.locale;
 
-		switch(args.length) {
-			case 0:
-				message.reply(this.rollDie(6));
-				break;
-
-			case 1:
-				const regex = /\d+/g;
-				const matchesCount = [...args[0].matchAll(regex)].length;
-
-				if (matchesCount !== 1) {
-					return Promise.reject(new CommandRejection(message, {
-						reason: `${this.domain}.invalidArg`,
-						args: args,
-					}));
-				}
-
-				message.reply(this.rollDie(args[0].match(regex)));
-				break;
-
-			default:
-				message.reply(this.parseFormula(args));
+		if (args.length < 1) {
+			return message.reply(this.rollDie(6));
 		}
+
+		if (args.some(a => [...a.matchAll(/^\d?d\d+$/g)].length < 1)) {
+			return Promise.reject(new CommandRejection(message, {
+				reason: `${this.domain}.invalidFormular`,
+				args: [args.join(' ')],
+			}));
+		}
+
+		message.reply(this.parseFormula(args));
 	}
 }
 
