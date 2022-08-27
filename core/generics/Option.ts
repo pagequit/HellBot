51d6f222c1501b6432b9cc65a81677895835b9cf
gambit <gambit@CHANGE>
2022-08-27 13:12:36 +0200

    Fix option type context issue

--- core/generics/Option.ts
+++ core/generics/Option.ts
@@ -17,11 +17,11 @@ export default class Option<T> {
 	}
 
 	isNone(): boolean {
-		return isNone(this.value);
+		return isNone.call(this, this.value);
 	}
 
 	isSome(): boolean {
-		return isSome(this.value);
+		return isSome.call(this, this.value);
 	}
 
 	unwrap(): T {
