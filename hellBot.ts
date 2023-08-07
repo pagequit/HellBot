2826bd7edd5a03c801ec4d592e13e4d07164312e
Kolaflash <kolaflash@NATSU>
2023-08-07 03:28:38 +0200

    WIP

--- hellBot.ts
+++ hellBot.ts
@@ -1,5 +1,5 @@
 import "https://deno.land/std@0.197.0/dotenv/load.ts";
-import { Client, Events, GatewayIntentBits } from "npm:discord.js";
+import { Client, Events, GatewayIntentBits } from "npm:discord.js@14.12.1";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
