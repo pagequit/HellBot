07e9cfd259ce8ccfa0e5ee0f537bb7b524657fea
kolaflash <kolaflash@GAMBIT>
2023-12-03 22:29:14 +0100

    debugDeno2

--- config.ts
+++ config.ts
@@ -3,18 +3,18 @@ import { load } from "std/dotenv/mod.ts";
 const env = await load();
 
 export const path = {
-  features: `${Deno.cwd()}/${env.FEATURES_PATH}`,
+  features: `${Deno.cwd()}/${
+    env.FEATURES_PATH ?? Deno.env.get("FEATURES_PATH")
+  }`,
 };
 
-console.log(path.features);
-
 export const discord = {
-  token: env.DISCORD_TOKEN,
-  guildId: env.HELLNET_ID,
-  clientId: env.HELLBOT_ID,
+  token: env.DISCORD_TOKEN ?? Deno.env.get("DISCORD_TOKEN"),
+  guildId: env.HELLNET_ID ?? Deno.env.get("HELLNET_ID"),
+  clientId: env.HELLBOT_ID ?? Deno.env.get("HELLBOT_ID"),
 };
 
 export const botlog = {
-  id: env.BOTLOG_ID,
-  token: env.BOTLOG_TOKEN,
+  id: env.BOTLOG_ID ?? Deno.env.get("BOTLOG_ID"),
+  token: env.BOTLOG_TOKEN ?? Deno.env.get("BOTLOG_TOKEN"),
 };
