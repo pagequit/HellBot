94f25ddaa286e80ad940d02be20d10d3aac0832e
kolaflash <kolaflash@GAMBIT>
2024-02-05 23:11:16 +0100

    try mistral again

--- features/llm/promt.ts
+++ features/llm/promt.ts
@@ -1,6 +1,6 @@
 import { Err, Ok, Result } from "unwrap";
-import { load } from "std/dotenv/mod.ts";
-import OpenAI from "openai";
+// import { load } from "std/dotenv/mod.ts";
+// import OpenAI from "openai";
 
 export function postJSON(
   url: URL,
@@ -19,25 +19,24 @@ export function postJSON(
     .catch((err) => Err<string, Error>(err));
 }
 
-// export default function (prompt: string): Promise<Result<string, Error>> {
-//   return postJSON(new URL("http://localhost:8000/completion"), {
-//     n_predict: 256,
-//     temperature: 0.7,
-//     prompt,
-//   });
-// }
-
-export default async function (promt: string): Promise<Result<string, Error>> {
-  const env = await load();
-  const openai = new OpenAI({
-    apiKey: Deno.env.get("OPENAI_API_KEY") ?? env.OPENAI_API_KEY,
+export default function (prompt: string): Promise<Result<string, Error>> {
+  return postJSON(new URL("http://localhost:8000/completion"), {
+    n_predict: 256,
+    temperature: 0.7,
+    prompt,
   });
-
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: promt }],
-  });
-
-  console.log(response);
-  return Ok(response.choices[0].message?.content ?? "");
 }
+
+// export default async function (promt: string): Promise<Result<string, Error>> {
+//   const env = await load();
+//   const openai = new OpenAI({
+//     apiKey: Deno.env.get("OPENAI_API_KEY") ?? env.OPENAI_API_KEY,
+//   });
+
+//   const response = await openai.chat.completions.create({
+//     model: "gpt-3.5-turbo",
+//     messages: [{ role: "user", content: promt }],
+//   });
+
+//   return Ok(response.choices[0].message?.content ?? "");
+// }
