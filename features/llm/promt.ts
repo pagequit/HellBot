b2c599796d80005ac5aa59fa1157f12d5cbe75a4
kolaflash <kolaflash@GAMBIT>
2024-02-05 23:30:10 +0100

    test

--- features/llm/promt.ts
+++ features/llm/promt.ts
@@ -20,7 +20,8 @@ export function postJSON(
 }
 
 export default function (prompt: string): Promise<Result<string, Error>> {
-  return postJSON(new URL("http://localhost:8000/completion"), {
+  // return postJSON(new URL("http://localhost:8000/completion"), {
+  return postJSON(new URL("http://llm:8000/completion"), {
     n_predict: 256,
     temperature: 0.7,
     prompt,
