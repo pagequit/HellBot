6d8d6d4196098bfe408a100e069af1fd65ca39ea
Kolaflash <kolaflash@pop-os.localdomain>
2024-02-23 10:39:40 +0100

    WIP

--- features/llm/chat/chats.ts
+++ features/llm/chat/chats.ts
@@ -1,53 +1,53 @@
 import { Collection, Ok, type Result } from "unwrap";
 import { Model } from "../Model.ts";
 import { type ChatBody } from "../ChatBody.ts";
-import gemini from "../handler/gemini.ts";
-import openAI from "../handler/openAI.ts";
 
 export type Message = {
   role: "user" | "system" | "assistant";
   content: string;
 };
 
-const chats = new Collection<string, { sendMessage: typeof sendMessage }>();
+class Chat {
+  sessionId: string;
+  handler: () => void;
+  context: Array<Message>;
 
-async function sendMessage(
-  this: Message[],
-  content: string,
-): Promise<Result<string, Error>> {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  constructor(sessionId: string, handler: Chat["handler"]) {
+    this.sessionId = sessionId;
+    this.handler = handler;
+    this.context = [];
+  }
 
-  this.push({ role: "user", content });
-  this.push({ role: "assistant", content: "Hello!" });
+  async sendMessage(content: string) {
+    const response = await this.handler.sendMessage(content);
+    let reply = "";
+    if (response.isOK) {
+      reply = response.body.text;
+    }
 
-  return Ok(content);
+    return reply;
+  }
 }
 
-const Mode: Record<
-  Model,
-  () => (sessionId: string, content: string) => Promise<ChatBody>
-> = {
-  [Model.Gemini]: googleChat,
-  [Model.ChatGPT]: openAIChat,
-  [Model.Mistral]: openAIChat,
+const chats = new Collection<string, Chat>();
+
+const Mode: Record<Model, (sessionId: string) => Chat> = {
+  [Model.Gemini]: (sessionId) => {
+    return new Chat(sessionId);
+  },
+  [Model.ChatGPT]: (sessionId) => {
+    return new Chat(sessionId);
+  },
+  [Model.Mistral]: (sessionId) => {
+    return new Chat(sessionId);
+  },
 };
 
-function googleChat() {
-  return gemini;
-}
+export function startChat(model: Model, sessionId: string) {
+  const chat = Mode[model](sessionId);
+  chats.set(sessionId, chat);
 
-function openAIChat() {
-  return openAI;
-}
-
-export function startChat(model: Model) {
-  Mode[model]();
-
-  const context: Message[] = [];
-
-  return {
-    sendMessage: sendMessage.bind(context),
-  };
+  return chat;
 }
 
 export function getChat(sessionId: string) {
