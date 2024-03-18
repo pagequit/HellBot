639660cbcddb7b648828df021bdeee478d6c1918
kolaflash <kolaflash@GAMBIT>
2024-03-18 23:27:07 +0100

    add grid stuff

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="grid">
+    <div class="col-12">
+      <div class="spinner">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.grid {
+  background-color: var(--background-2);
+  padding: var(--sp-3);
+}
+
+.col-1,
+.col-2,
+.col-3,
+.col-4,
+.col-5,
+.col-6,
+.col-7,
+.col-8,
+.col-9,
+.col-10,
+.col-11,
+.col-12 {
+  background-color: var(--c-blurple);
+  color: var(--c-white);
+  text-align: center;
+  padding: var(--sp-3);
+  margin-bottom: var(--sp-3);
+  font-weight: bold;
+  font-size: larger;
 }
 </style>
