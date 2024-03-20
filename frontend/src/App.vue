a99f8a0689389fa5eb1d72e0d2f81a01490be2a9
kolaflash <kol@flash>
2024-03-20 23:52:28 +0100

    Add more styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import DieBestie from "./components/icons/DieBestie.vue";
 </script>
 
 <template>
-  <header>
+  <header class="header">
+      <a class="home" href="/">
+        <DieBestie class="logo"/>
+        <span>HellNet</span>
+      </a>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -12,3 +17,31 @@ import { RouterLink, RouterView } from "vue-router";
 
   <RouterView />
 </template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--sp-3);
+  background-color: var(--c-bg-1);
+
+  .home {
+    font-size: larger;
+    font-weight: bold;
+    color: var(--c-fg-1);
+    text-decoration: none;
+    flex: 0 1 auto;
+    justify-content: start;
+    display: flex;
+    align-items: center;
+    width: fit-content;
+  }
+
+  .logo {
+    width: 32px;
+    height: 32px;
+    margin-right: var(--sp-2);
+  }
+}
+</style>
