e4b9b82b3f57037ffc8b5ed6dc44d509e76500ef
kolaflash <kolaflash@GAMBIT>
2024-04-14 02:25:46 +0200

    Add logout route

--- core/http/Server.ts
+++ core/http/Server.ts
@@ -20,7 +20,7 @@ export default new Elysia()
     }
 
     auth.set({
-      value: await jwt.sign({ id: user.unwrap(), ...params }),
+      value: await jwt.sign({ id: user.unwrap() }),
       httpOnly: true,
       maxAge: 7 * 86400,
       path: "/",
@@ -30,6 +30,24 @@ export default new Elysia()
     crappyStore.delete(params.token);
 
     return `Sign in as ${user.unwrap()}`;
+  })
+  .get("/logout", async ({ jwt, set, cookie: { auth } }) => {
+    const user = await jwt.verify(auth.value);
+
+    if (!user) {
+      set.status = 401;
+      return "Unauthorized";
+    }
+
+    auth.set({
+      value: await jwt.sign({ id: user.id }),
+      httpOnly: true,
+      maxAge: 0,
+      path: "/",
+      secure: false, // FIXME for production
+    });
+
+    return `Bye ${user.id}`;
   })
   .get("/", async ({ jwt, set, cookie: { auth } }) => {
     const user = await jwt.verify(auth.value);
