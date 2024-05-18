54c062e4a268c0eaa11cbf16dc8f0a1de370c0f3
kolaflash <kolaflash@GAMBIT>
2024-05-18 16:18:04 +0200

    Remove poll since discord ship this feature now

--- features/poll/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Feature } from "@/core/Feature.ts";
-import { registerChatInputGuildCommand } from "@/core/mod.ts";
-import { poll } from "./poll.ts";
-
-export default ((): void => {
-  registerChatInputGuildCommand(poll);
-}) satisfies Feature;
--- features/poll/translations/en.ts
+++
@@ -1,9 +0,0 @@
-import type { RawTranslation } from "@/core/i18n/I18n.ts";
-
-export default {
-  name: () => "poll",
-  description: () => "Creates a simple poll.",
-  subjects: () => "subjects",
-  subjectsDescription: () => "A whitespace separated list of subjects.",
-  replyExeption: () => "A maximum of nine subjects may be specified.",
-} satisfies RawTranslation;
