303e980c6157fc9151e1e163c7eb13601f22b5f4
Zinnusl <Zinnusl@draig.de>
2024-05-22 10:47:26 +0200

    Feat: Added japanese translations

---
+++ frontend/src/views/chat/translations/jp.ts
@@ -0,0 +1,6 @@
+import type { RawTranslation } from "@/core/i18n/I18n.ts";
+
+export default {
+    promptPlaceholder: () => "ここにプロンプトを入力してください ...",
+    submitTitle: () => "送信",
+} satisfies RawTranslation;
