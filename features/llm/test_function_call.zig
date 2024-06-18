9903bfa283df7316908be1905222966952196df1
kolaflash <kol@flash>
2024-06-18 19:07:01 +0200

    WIP

--- features/llm/test_function_call.zig
+++ features/llm/test_function_call.zig
@@ -11,3 +11,27 @@ test "while if bla test" {
     try expect(sum == 4);
 }
 
+test "defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    try expect(x == 4.5);
+}
+
+const AllocationError = error{OutOfMemory};
+
+test "coerce error from a subset to a superset" {
+    const err: FileOpenError = AllocationError.OutOfMemory;
+    try expect(err == FileOpenError.OutOfMemory);
+}
+
+test "error union" {
+    const maybe_error: AllocationError!u16 = 10;
+    cont no_error = maybe_error catch 0;
+
+    try expect(@TypeOf(no_error) == u16);
+    try expect(no_error == 10);
+}
+
