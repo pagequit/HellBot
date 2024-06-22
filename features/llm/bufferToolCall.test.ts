776b0552b54b02f027f60a0d939d81beae11118d
kolaflash <kolaflash@GAMBIT>
2024-06-23 00:27:23 +0200

    WIP

--- features/llm/bufferToolCall.test.ts
+++ features/llm/bufferToolCall.test.ts
@@ -1,39 +1,24 @@
 import { expect, test } from "bun:test";
-type ToolCallTemplate = {
-  index: number;
-  template: string;
-};
-
-const toolCallStart = {
-  index: 0,
-  template: "<tool_call>",
-} satisfies ToolCallTemplate;
-
-const toolCallEnd = {
-  index: 0,
-  template: "</tool_call>",
-} satisfies ToolCallTemplate;
-
-const toolCallTurns = [toolCallStart, toolCallEnd];
-let toolCallTurn = 0;
 
 function bufferToolCall(buffer: { ref: string }): (chunk: string) => void {
+  const templates = ["<tool_call>", "</tool_call>"];
+  let turn = 0;
+  let index = 0;
+
   return (chunk: string) => {
     for (const char of chunk) {
-      const toolCall = toolCallTurns[toolCallTurn];
-
-      if (toolCall.template[toolCall.index] === char) {
-        toolCallTurns[toolCallTurn].index += 1;
-      } else if (toolCallTurn === 1) {
+      if (templates[turn][index] === char) {
+        index += 1;
+      } else if (turn === 1) {
         buffer.ref += char;
-        console.log(buffer.ref);
       }
 
-      if (toolCall.index === toolCall.template.length) {
-        toolCallTurn += 1;
-        toolCall.index = 0;
-        if (toolCallTurn === 2) {
-          toolCallTurn = 0;
+      if (index === templates[turn].length) {
+        turn += 1;
+        index = 0;
+
+        if (turn === 2) {
+          turn = 0;
           return;
         }
       }
@@ -41,7 +26,7 @@ function bufferToolCall(buffer: { ref: string }): (chunk: string) => void {
   };
 }
 
-test("testBufferToolCall happy string", () => {
+test("testBufferToolCall", () => {
   const buffer = { ref: "" };
   const parse = bufferToolCall(buffer);
 
@@ -63,17 +48,4 @@ test("testBufferToolCall happy string", () => {
   }
 
   expect(buffer.ref).toBe("hello");
-});
-
-test("testBufferToolCall stupid string", () => {
-  const buffer = { ref: "" };
-  const parse = bufferToolCall(buffer);
-
-  for (const char of '<tool_call> {"arguments": {"symbol": "TSLA"}, "name": "get_stock_fundamentals"} </tool_call>') {
-    parse(char);
-  }
-
-  expect(buffer.ref).toBe(
-    ' {"arguments": {"symbol": "TSLA"}, "name": "get_stock_fundamentals"} ',
-  );
 });
